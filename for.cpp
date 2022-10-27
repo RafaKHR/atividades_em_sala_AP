843ad6f4b6886f9fed9a309cc84e79ab1cca5919
Rafael <111534933+RafaKHR@users.noreply.github.com>
2022-10-27 12:35:23 -0300

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int i;
+	
+	for (i = 0;  i < 10000; i++){ // CHAVE OPCIONAL, SE HOUVER MAIS DE UM COMANDO É NECESSÁRIO, VARIAVEL PARA O FOR PODE SER QUALQUER UMA, MAIS USADO "i"
+		printf("Os alunos de Algoritmo são os mais lindos"); 
+		printf(" e inteligentes da UCB\n");	
+	}
+}
