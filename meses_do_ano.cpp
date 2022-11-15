85394fb640e46207ce7f19f622865caa1366fb06
Rafael <111534933+RafaKHR@users.noreply.github.com>
2022-11-14 21:08:52 -0300

    Rename Dias do mês.cpp to meses_do_ano.cpp

--- meses_do_ano.cpp
+++ meses_do_ano.cpp
@@ -7,7 +7,7 @@ int main(){
 	switch (mes){
 		case 1: printf("Janeiro"); break;
 		case 2: printf("Fevereiro"); break;
-		case 3: printf("Março"); break;
+		case 3: printf("MarÃ§o"); break;
 		case 4: printf("Abril"); break;
 		case 5: printf("Maio"); break;
 		case 6: printf("Junho"); break;
